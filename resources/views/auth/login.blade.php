eb74e9fb03dc0b3395926dbec755d5decc6edb3f
Hermes1823 <hermescallao47@gmail.com>
2023-12-11 16:31:10 -0500

    configurando inicio de sesion
    login y dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,9 +39,15 @@
                     </a>
                 @endif
 
+                <x-button class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Register') }}
+                </x-button>
+
                 <x-button class="ms-4">
                     {{ __('Log in') }}
                 </x-button>
+
+            
             </div>
         </form>
     </x-authentication-card>
